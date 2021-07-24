2d8ba93eee56da8a105474def4abcc58795349df
kondounagi <kondounagi@gmail.com>
2021-07-25 00:21:16 +0900

    chore: remove version specification in requirements

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
     classifiers=[
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3"
     ],
 )
